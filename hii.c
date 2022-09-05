3df8742336ecf18964a3451b900030fac6b786dd
ru0ya <viktaangi02@gmail.com>
2022-09-05 20:47:03 +0300

    check tis

--- hii.c
+++ hii.c
@@ -9,19 +9,20 @@ int process(char *buffer, char **argv)
 {
         pid_t pid;
         pid = fork();
+
         if (pid == -1)
                 return (-1);
-if (pid == 0)
-{
-int x = execve(buffer, argv, NULL);
-if (x == -1)
-{
-perror("error");
-exit(EXIT_FAILURE);
-}
-execve(buffer, argv, NULL);
-}
-return (1);
+	if (pid == 0)
+	{
+		int x = execve(buffer, argv, NULL);
+		if (x == -1)
+		{
+			perror("error");
+			exit(EXIT_FAILURE);
+		}
+		execve(buffer, argv, NULL);
+	}
+	return (1);
 }
 /**
 main - entry point
@@ -33,19 +34,17 @@ main - entry point
 int main(int argc, char *argv[])
 {
 	char *buffer = NULL;
-size_t i = 1024;
-int status = 1;
-char *str;
-pid_t pid;
-do {
-printf("$ ");
-getline(&buffer, &i, stdin);
-str=strtok(buffer, "\n");
-status = process(str, argv);
-printf("this is %s\n", buffer);
-} while (status);
-return (status);
+	size_t i = 1024;
+	int status = 1;
+	char *str;
+	pid_t pid;
+	do 
+	{
+		printf("$ ");
+		getline(&buffer, &i, stdin);
+		str=strtok(buffer, "\n");
+		status = process(str, argv);
+		printf("this is %s\n", buffer);
+	} while (status);
+	return (status);
 }
-
-                                    
-
