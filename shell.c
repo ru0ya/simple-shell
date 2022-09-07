11d44f3b66944026ff1c91282ada1d4175574673
theeduke <oscarian528@gmail.com>
2022-09-07 13:32:44 -0700

    task 1

--- shell.c
+++ shell.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /*
  * process - returns a process
  * @buffer: pointer to a buffer
@@ -7,8 +8,8 @@
  */
 int process(char *buffer, char **argv)
 {
-        pid_t pid;
-        pid = fork();
+	pid_t pid;
+	pid = fork();
         if (pid == -1)
                 return (-1);
 	if (pid == 0)
@@ -34,7 +35,7 @@ int main(int argc, char *argv[])
 {
 	char *buffer = NULL;
 	size_t i = 1024;
-	int status = 1;
+	int status = 1, p = 0;
 	char *str;
 	pid_t pid;
 
@@ -42,6 +43,13 @@ int main(int argc, char *argv[])
 		printf("$ ");
 		getline(&buffer, &i, stdin);
 		str=strtok(buffer, "\n");
+		while (str != NULL)
+		{
+			argv[p] = str;
+			p++;
+			str = strtok(NULL, "\N");
+		}
+		argv[P] = NULL;
 		status = process(str, argv);
 		printf("this is %s\n", buffer);
 	} while (status);
